9e9f5eadb11768c01ce135e4eabf1f2070a13a87
Drish <drish.xd@gmail.com>
2021-12-28 12:02:30 +0530

    fix routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 
 const routes = [
@@ -19,6 +19,7 @@ const routes = [
 ];
 
 const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
